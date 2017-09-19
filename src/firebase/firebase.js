8bde1ff31e793bc71bf06854e0f27b28747e4508
Robin Wieruch <wrobin@gmx.net>
2017-09-19 17:21:58 +0200

    init

---
+++ src/firebase/firebase.js
@@ -0,0 +1,35 @@
+import * as firebase from 'firebase';
+
+const prodConfig = {
+  apiKey: 'AIzaSyBtxDyenAXZ3phPeXcsZs6hoIXa7n33NtQ',
+  authDomain: 'react-firebase-authentic-d64f8.firebaseapp.com',
+  databaseURL: 'https://react-firebase-authentic-d64f8.firebaseio.com',
+  projectId: 'react-firebase-authentic-d64f8',
+  storageBucket: '',
+  messagingSenderId: '705898054501'
+};
+
+const devConfig = {
+  apiKey: 'AIzaSyBtxDyenAXZ3phPeXcsZs6hoIXa7n33NtQ',
+  authDomain: 'react-firebase-authentic-d64f8.firebaseapp.com',
+  databaseURL: 'https://react-firebase-authentic-d64f8.firebaseio.com',
+  projectId: 'react-firebase-authentic-d64f8',
+  storageBucket: '',
+  messagingSenderId: '705898054501'
+};
+
+const config = process.env.NODE_ENV === 'production'
+  ? prodConfig
+  : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export {
+  db,
+  auth,
+};
