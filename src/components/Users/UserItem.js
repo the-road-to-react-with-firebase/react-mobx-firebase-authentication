3c97f29600de58a3199c981687c49ae45fa3193b
Robin Wieruch <wrobin@gmx.net>
2018-12-04 12:04:33 +0800

    refactor Admin and Users

---
+++ src/components/Users/UserItem.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
+
+class UserItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    if (!this.props.user) {
+      this.setState({ loading: true });
+    }
+
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .on('value', snapshot => {
+        this.props.userStore.setUser(
+          snapshot.val(),
+          this.props.match.params.id,
+        );
+
+        this.setState({ loading: false });
+      });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+  }
+
+  onSendPasswordResetEmail = () => {
+    this.props.firebase.doPasswordReset(this.props.user.email);
+  };
+
+  render() {
+    const user = (this.props.userStore.users || {})[
+      this.props.match.params.id
+    ];
+    const { loading } = this.state;
+
+    return (
+      <div>
+        <h2>User ({this.props.match.params.id})</h2>
+        {loading && <div>Loading ...</div>}
+
+        {user && (
+          <div>
+            <span>
+              <strong>ID:</strong> {user.uid}
+            </span>
+            <span>
+              <strong>E-Mail:</strong> {user.email}
+            </span>
+            <span>
+              <strong>Username:</strong> {user.username}
+            </span>
+            <span>
+              <button
+                type="button"
+                onClick={this.onSendPasswordResetEmail}
+              >
+                Send Password Reset
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default compose(
+  withFirebase,
+  inject('userStore'),
+  observer,
+)(UserItem);
---
+++ src/components/Users/UserList.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+class UserList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    if (!this.props.userStore.userList.length) {
+      this.setState({ loading: true });
+    }
+
+    this.props.firebase.users().on('value', snapshot => {
+      this.props.userStore.setUsers(snapshot.val());
+
+      this.setState({ loading: false });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    const users = this.props.userStore.userList;
+    const { loading } = this.state;
+
+    return (
+      <div>
+        <h2>Users</h2>
+        {loading && <div>Loading ...</div>}
+        <ul>
+          {users.map(user => (
+            <li key={user.uid}>
+              <span>
+                <strong>ID:</strong> {user.uid}
+              </span>
+              <span>
+                <strong>E-Mail:</strong> {user.email}
+              </span>
+              <span>
+                <strong>Username:</strong> {user.username}
+              </span>
+              <span>
+                <Link to={`${ROUTES.ADMIN}/${user.uid}`}>
+                  Details
+                </Link>
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default compose(
+  withFirebase,
+  inject('userStore'),
+  observer,
+)(UserList);
